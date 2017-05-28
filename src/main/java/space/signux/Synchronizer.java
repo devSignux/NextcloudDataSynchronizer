5139d212c8a12893a92face985b0abad535862bb
signux <signux@gmx.net>
2017-05-28 18:17:52 +0200

    read settings from commandline parameter added

--- src/main/java/space/signux/Synchronizer.java
+++ src/main/java/space/signux/Synchronizer.java
@@ -13,24 +13,40 @@ public class Synchronizer {
 
 	public static void main(String[] args) {
 		Synchronizer synchronizer = new Synchronizer();
-		synchronizer.loadSettings();
+		if (!synchronizer.loadSettings(args)) {
+			return;
+		}
+
 	}
 
-	private void loadSettings() {
+	private boolean loadSettings(String[] args) {
+
 		String filename = "settings.txt";
-		File propertiesFile = new File(filename);
+		if (args.length > 0) {
+			filename = args[0];
+		}
+		File settingsFile = new File(filename);
+
+		if (!settingsFile.exists()) {
+			System.out.println("settings file: " + filename + " doesn't exist!");
+			return false;
+		}
+
 		FileInputStream in;
 		try {
-			in = new FileInputStream(propertiesFile);
+			System.out.println("load settings from file: " + filename);
+			in = new FileInputStream(settingsFile);
 			Properties properties = new Properties();
 			properties.load(in);
 			inputConnection = SettingsReader.CreateInputConnectionSettings(properties);
 			outputConnection = SettingsReader.CreateOutputConnectionSettings(properties);
 		} catch (FileNotFoundException e) {
-			System.out.println("Can't find file " + filename);
+			System.out.println("Can't find file: " + filename);
+			return false;
 		} catch (IOException e) {
-			System.out.println("Can't load file " + filename);
+			System.out.println("Can't load file: " + filename);
+			return false;
 		}
-
+		return true;
 	}
 }
